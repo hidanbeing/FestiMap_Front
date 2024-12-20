2ab6f45bc4277d436e938fabe8d6711cff59b04a
hidanbeing <nd6162bv@gmail.com>
2024-12-21 00:22:26 +0900

    add: map data 추가

---
+++ components/data/MapData.js
@@ -0,0 +1,440 @@
+// MapData.js
+
+export const regions = [
+  {
+    id: 1,
+    coordinates: [
+      { latitude: 37.5265, longitude: 126.9265 },
+      { latitude: 37.5265, longitude: 126.9305 },
+      { latitude: 37.5285, longitude: 126.9305 },
+      { latitude: 37.5285, longitude: 126.9265 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)",
+  },
+  {
+    id: 2,
+    coordinates: [
+      { latitude: 37.5265, longitude: 126.9265 },
+      { latitude: 37.5265, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.9265 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)",
+  },
+  {
+    id: 3,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9265 },
+      { latitude: 37.5305, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.9265 },
+    ],
+
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 4,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9265 },
+      { latitude: 37.5305, longitude: 126.92 },
+      { latitude: 37.5345, longitude: 126.92 },
+      { latitude: 37.533, longitude: 126.9265 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+
+  {
+    id: 5,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9155 },
+      { latitude: 37.5305, longitude: 126.92 },
+      { latitude: 37.5345, longitude: 126.92 },
+      { latitude: 37.5355, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 6,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9155 },
+      { latitude: 37.5305, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 7,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9155 },
+      { latitude: 37.5305, longitude: 126.91 },
+      { latitude: 37.5285, longitude: 126.911 },
+      { latitude: 37.5285, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 8,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9155 },
+      { latitude: 37.5305, longitude: 126.91 },
+      { latitude: 37.5355, longitude: 126.911 },
+      { latitude: 37.5354, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 9,
+    coordinates: [
+      { latitude: 37.5215, longitude: 126.9155 },
+      { latitude: 37.5215, longitude: 126.914 },
+      { latitude: 37.5285, longitude: 126.91 },
+      { latitude: 37.5285, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 10,
+    coordinates: [
+      { latitude: 37.5255, longitude: 126.9155 },
+      { latitude: 37.5255, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.92 },
+      { latitude: 37.5285, longitude: 126.9155 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 11,
+    coordinates: [
+      { latitude: 37.5255, longitude: 126.9155 },
+      { latitude: 37.5255, longitude: 126.92 },
+      { latitude: 37.5205, longitude: 126.92 },
+      { latitude: 37.5205, longitude: 126.9155 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 12,
+    coordinates: [
+      { latitude: 37.5225, longitude: 126.92 },
+      { latitude: 37.5225, longitude: 126.922 },
+      { latitude: 37.5245, longitude: 126.922 },
+      { latitude: 37.5245, longitude: 126.92 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+  {
+    id: 13,
+    coordinates: [
+      { latitude: 37.5265, longitude: 126.92 },
+      { latitude: 37.5265, longitude: 126.9245 },
+      { latitude: 37.5245, longitude: 126.9245 },
+      { latitude: 37.5245, longitude: 126.92 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 빨강 (높은 혼잡도)
+  },
+
+  {
+    id: 14,
+    coordinates: [
+      { latitude: 37.5285, longitude: 126.9305 },
+      { latitude: 37.5285, longitude: 126.9345 },
+      { latitude: 37.531, longitude: 126.9305 },
+      { latitude: 37.5305, longitude: 126.9305 },
+    ],
+    color: "rgba(255, 0, 0, 0.3)", // 주황 (중간 혼잡도)
+  },
+  {
+    id: 15,
+    coordinates: [
+      { latitude: 37.5285, longitude: 126.9265 },
+      { latitude: 37.5285, longitude: 126.9305 },
+      { latitude: 37.531, longitude: 126.9305 },
+      { latitude: 37.5305, longitude: 126.9265 },
+    ],
+    color: "rgba(255, 127, 0, 0.5)", // 초록 (낮은 혼잡도)
+
+    // color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 16,
+    coordinates: [
+      { latitude: 37.5305, longitude: 126.9265 },
+      { latitude: 37.531, longitude: 126.9305 },
+      { latitude: 37.5328, longitude: 126.9265 },
+      { latitude: 37.5328, longitude: 126.9265 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+  },
+  {
+    id: 17,
+    coordinates: [
+      { latitude: 37.5265, longitude: 126.9345 },
+      { latitude: 37.5265, longitude: 126.9365 },
+      { latitude: 37.5285, longitude: 126.9345 },
+      { latitude: 37.5285, longitude: 126.9345 },
+    ],
+    color: "rgba(255, 0, 0, 0.3)", // 노랑 (보통 혼잡도)
+  },
+  {
+    id: 18,
+    coordinates: [
+      { latitude: 37.5245, longitude: 126.9285 },
+      { latitude: 37.5245, longitude: 126.9325 },
+      { latitude: 37.5265, longitude: 126.9325 },
+      { latitude: 37.5265, longitude: 126.9285 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)", // 보라 (특정 혼잡도)
+  },
+  {
+    id: 19,
+    coordinates: [
+      { latitude: 37.5265, longitude: 126.9245 },
+      { latitude: 37.5265, longitude: 126.9285 },
+      { latitude: 37.5245, longitude: 126.9285 },
+      { latitude: 37.5245, longitude: 126.9245 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)", // 청록 (매우 낮음)
+  },
+  {
+    id: 20,
+    coordinates: [
+      // 노
+      { latitude: 37.5265, longitude: 126.9305 },
+      { latitude: 37.5265, longitude: 126.9345 },
+      { latitude: 37.5285, longitude: 126.9345 },
+      { latitude: 37.5285, longitude: 126.9305 },
+    ],
+    color: "rgba(255, 0, 0, 0.3)", // 보라 (특정 혼잡도)
+  },
+  {
+    id: 21,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9345 },
+      { latitude: 37.5225, longitude: 126.9305 },
+      { latitude: 37.5205, longitude: 126.9305 },
+      { latitude: 37.5205, longitude: 126.9345 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 보라 (특정 혼잡도)
+  },
+  {
+    id: 22,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9345 },
+      { latitude: 37.5225, longitude: 126.9305 },
+      { latitude: 37.5245, longitude: 126.9305 },
+      { latitude: 37.5245, longitude: 126.9345 },
+    ],
+    color: "rgba(255, 255, 0, 0.3)", // 청록?? (매우 낮음)
+  },
+  {
+    id: 23,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9415 },
+      { latitude: 37.5225, longitude: 126.9385 },
+      { latitude: 37.5245, longitude: 126.9385 },
+      { latitude: 37.5245, longitude: 126.939 },
+    ],
+    color: "rgba(255, 127, 0, 0.7)", // 초록 (낮은 혼잡도)
+    // color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+  },
+  {
+    id: 24,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9385 },
+      { latitude: 37.5225, longitude: 126.9345 },
+      { latitude: 37.5245, longitude: 126.9345 },
+      { latitude: 37.5245, longitude: 126.9385 },
+    ],
+    // color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+    color: "rgba(255, 0, 0, 0.3)", // 청록 (매우 낮음)
+  },
+  {
+    id: 25,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9385 },
+      { latitude: 37.5225, longitude: 126.9345 },
+      { latitude: 37.5205, longitude: 126.9345 },
+      { latitude: 37.5205, longitude: 126.9385 },
+    ],
+    // color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+    color: "rgba(255, 0, 0, 0.3)", // 청록 (매우 낮음)
+  },
+
+  {
+    id: 26,
+    coordinates: [
+      // 노
+      { latitude: 37.5265, longitude: 126.9365 },
+      { latitude: 37.5265, longitude: 126.9325 },
+      { latitude: 37.5245, longitude: 126.9325 },
+      { latitude: 37.5245, longitude: 126.939 },
+    ],
+    // color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+    color: "rgba(255, 127, 0, 0.7)", // 초록 (낮은 혼잡도)
+  },
+
+  {
+    id: 27,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.9305 },
+      { latitude: 37.5225, longitude: 126.927 },
+      { latitude: 37.5245, longitude: 126.927 },
+      { latitude: 37.5245, longitude: 126.9305 },
+    ],
+    // color: "rgba(255, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+    color: "rgba(255, 255, 0, 0.3)", // 청록 (매우 낮음)
+  },
+  {
+    id: 28,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.922 },
+      { latitude: 37.5205, longitude: 126.927 },
+      { latitude: 37.5225, longitude: 126.927 },
+      { latitude: 37.5225, longitude: 126.922 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 노랑 (보통 혼잡도)
+    // color: "rgba(255, 0, 0, 0.3)", // 청록 (매우 낮음)
+  },
+  {
+    id: 29,
+    coordinates: [
+      // 노
+      { latitude: 37.5225, longitude: 126.922 },
+      { latitude: 37.5225, longitude: 126.927 },
+      { latitude: 37.5245, longitude: 126.927 },
+      { latitude: 37.5245, longitude: 126.922 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+
+  {
+    id: 30,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.922 },
+      { latitude: 37.5205, longitude: 126.92 },
+      { latitude: 37.5225, longitude: 126.92 },
+      { latitude: 37.5225, longitude: 126.922 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 31,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.927 },
+      { latitude: 37.5205, longitude: 126.9305 },
+      { latitude: 37.5225, longitude: 126.9305 },
+      { latitude: 37.5225, longitude: 126.927 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 32,
+    coordinates: [
+      // 노
+      { latitude: 37.5165, longitude: 126.927 },
+      { latitude: 37.5165, longitude: 126.9305 },
+      { latitude: 37.5205, longitude: 126.9305 },
+      { latitude: 37.5205, longitude: 126.927 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 33,
+    coordinates: [
+      // 노
+      { latitude: 37.5165, longitude: 126.923 },
+      { latitude: 37.5165, longitude: 126.927 },
+      { latitude: 37.5205, longitude: 126.927 },
+      { latitude: 37.5205, longitude: 126.923 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 34,
+    coordinates: [
+      // 노
+      { latitude: 37.5165, longitude: 126.9305 },
+      { latitude: 37.5165, longitude: 126.935 },
+      { latitude: 37.5205, longitude: 126.935 },
+      { latitude: 37.5205, longitude: 126.9305 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 35,
+    coordinates: [
+      // 노
+      { latitude: 37.5165, longitude: 126.935 },
+      { latitude: 37.5165, longitude: 126.94 },
+      { latitude: 37.5205, longitude: 126.94 },
+      { latitude: 37.5205, longitude: 126.935 },
+    ],
+    // color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 36,
+    coordinates: [
+      // 노
+      { latitude: 37.5165, longitude: 126.935 },
+      { latitude: 37.5165, longitude: 126.945 },
+      { latitude: 37.5205, longitude: 126.94 },
+      { latitude: 37.5205, longitude: 126.935 },
+    ],
+    // color: "rgba(255, 100, 0, 0.3)", // 초록 (낮은 혼잡도)
+    color: "rgba(255, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 37,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.9385 },
+      { latitude: 37.5205, longitude: 126.944 },
+      { latitude: 37.5225, longitude: 126.942 },
+      { latitude: 37.5225, longitude: 126.9385 },
+    ],
+    color: "rgba(255, 0, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 38,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.94 },
+      { latitude: 37.5205, longitude: 126.944 },
+      { latitude: 37.517, longitude: 126.9443 },
+      { latitude: 37.519, longitude: 126.942 },
+    ],
+    color: "rgba(255, 127, 0, 0.7)", // 초록 (낮은 혼잡도)
+
+    // color: "rgba(255, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+  {
+    id: 39,
+    coordinates: [
+      // 노
+      { latitude: 37.5205, longitude: 126.918 },
+      { latitude: 37.5205, longitude: 126.923 },
+      { latitude: 37.517, longitude: 126.923 },
+      { latitude: 37.519, longitude: 126.918 },
+    ],
+    color: "rgba(0, 255, 0, 0.3)", // 초록 (낮은 혼잡도)
+  },
+];
+// Marker 데이터
+export const markers = [
+  {
+    id: 1,
+    coordinate: { latitude: 37.5265, longitude: 126.9295 },
+    title: "여의나루역",
+    description: "혼잡도가 높은 지역입니다.",
+  },
+];
